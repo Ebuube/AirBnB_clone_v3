ba48a8db954a31b914856bbae6c11669a19c3a7e
Ebuube <onwutaebubegideon1555@gmail.com>
2023-10-29 11:04:40 +0000

    Retrieve state by id

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,6 +7,7 @@ from flask import jsonify
 from models import storage
 from models.state import State
 from flask import make_response
+from flask import abort
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
@@ -20,3 +21,15 @@ def get_states():
         states.append(state.to_dict())
 
     return make_response(jsonify(states))
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state_by_id(state_id):
+    """
+    Retrieve a State object
+    """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+
+    return make_response(jsonify(state.to_dict()))
