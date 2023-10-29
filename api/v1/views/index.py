6a3e3c6a03053412f62b711c9bbc77e12ec8ae7c
Ebuube <onwutaebubegideon1555@gmail.com>
2023-10-29 10:27:46 +0000

    Task: some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,18 @@ Create index page
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.place import Place
+from models.city import City
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = [Amenity, Place, City, Review, State, User]
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def api_status():
     """
     Check if api is available or not
@@ -14,3 +23,15 @@ def api_status():
     status = {"status": "OK"}
 
     return jsonify(status)
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def api_stats():
+    """
+    Retrieve the number of each objects by type
+    """
+    stats = dict()
+    for cls in classes:
+        stats[cls.__tablename__] = storage.count(cls)
+
+    return jsonify(stats)
